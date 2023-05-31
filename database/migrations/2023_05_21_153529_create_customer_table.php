7d8d50a3a413e7d9a7a39727ee6bbb58963e99a4
ariyanahh19 <ariyanahh19@gmail.com>
2023-05-31 19:57:04 +0700

    commit awal

---
+++ database/migrations/2023_05_21_153529_create_customer_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('customer', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_pengguna')->nullable();
+            $table->string('nama_lengkap')->nullable();
+            $table->bigInteger('nomor_hp')->nullable();
+            $table->varchar('email')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('customer');
+    }
+};
